0ad2eea053c210660367b30dcfcf19c3be10f4c6
Simon Grossenbacher <s.grossenbacher@biral.ch>
2020-07-09 07:20:36 +0200

    add pwa properties

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/ParaBoard' : '/'
-}
+    publicPath: process.env.NODE_ENV === "production" ? "/ParaBoard" : "/",
+
+    pwa: {
+        name: 'ParaBoard',
+        workboxPluginMode: "GenerateSW",
+        themeColor: "#4DBA87",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black"
+    }
+};

c6e1cf56b2dbb5f459f078bfcda44ba8af032637
Simon Grossenbacher <s.grossenbacher@biral.ch>
2020-07-09 07:43:00 +0200

    let the production dist path also point to the root directory

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
+process.env.VUE_APP_VERSION = require('./package.json').version
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/ParaBoard" : "/",
+    publicPath: process.env.NODE_ENV === "production"
+        ? "/" 
+        : "/",
 
     pwa: {
         name: 'ParaBoard',
